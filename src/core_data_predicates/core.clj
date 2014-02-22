5aecc0bc4449a5371d72992ffb9983da0c481056
Terry Lewis <terrylewis@outlook.com>
2014-02-21 22:03:16 -0600

    @end not properly being added + new class names

--- src/core_data_predicates/core.clj
+++ src/core_data_predicates/core.clj
@@ -18,7 +18,7 @@
 
   (def int-imports (apply str ["#import <CoreData/CoreData.h>\n" "#import <Foundation/Foundation.h>\n\n"]))
 
-  (defn imp-imports [class-name] (apply str ["#import" "\"" class-name "+Operations.h\"\n\n"]))
+  (defn imp-imports [class-name] (apply str ["#import" "\"" class-name "+_FetchRequests.h\"\n\n"]))
 
   (def content (xml-from-file "Model.xcdatamodeld/Model.xcdatamodel/contents"))
 
@@ -28,13 +28,13 @@
 
   (defn is-equal-from-keypath-imp [entity key-path] (imp-with-entity-keypath entity key-path))
 
-  (defn int-file-name-from-class [class] (apply str [class "+Operations.h"]))
+  (defn int-file-name-from-class [class] (apply str [class "+_FetchRequests.h"]))
 
-  (defn imp-file-name-from-class [class] (apply str [class "+Operations.m"]))
+  (defn imp-file-name-from-class [class] (apply str [class "+_FetchRequests.m"]))
 
-  (defn inteface-dec [class-name] (apply str ["@inteface " class-name "(Operations)\n"]))
+  (defn inteface-dec [class-name] (apply str ["@inteface " class-name " (_FetchRequests)\n\n"]))
 
-  (defn imp-dec [class-name] (apply str ["@implementation " class-name "(Operations)\n"]))
+  (defn imp-dec [class-name] (apply str ["@implementation " class-name " (_FetchRequests)\n"]))
 
    (doseq [lines content]
      (doseq [other (:content lines)]
@@ -51,4 +51,11 @@
        )
      )
   )
+
+  (doseq [lines content]
+     (doseq [other (:content lines)]
+        (when-let [class-name (:name (:attrs other))]
+          (spit (int-file-name-from-class class-name) "\n@end]\n" :append true)
+          (spit (imp-file-name-from-class class-name) "\n@end\n" :append true)))
+       )
 )
