d81890bf2c3a0ed0beb6c2422857679ac63e5035
Terry Lewis <terrylewis@outlook.com>
2014-02-21 20:35:57 -0600

    inital commit

---
+++ test/core_data_predicates/core_test.clj
@@ -0,0 +1,7 @@
+(ns core-data-predicates.core-test
+  (:require [clojure.test :refer :all]
+            [core-data-predicates.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
