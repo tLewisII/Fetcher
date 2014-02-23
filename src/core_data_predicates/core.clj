217ad5d1811fb2d13ceb29622700fe0307a5cc22
Terry Lewis <terrylewis@outlook.com>
2014-02-22 22:24:13 -0600

    Removed uses of (apply str [...]) for string building

--- src/core_data_predicates/core.clj
+++ src/core_data_predicates/core.clj
@@ -20,34 +20,34 @@
 
   (defn xml-from-file [file] (zip/xml-zip (xml/parse file)))
 
-  (defn imp-with-entity-keypath [entity key-path] (apply str ["\tNSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@\""  entity "\"];\n"
+  (defn imp-with-entity-keypath [entity key-path] (str "\tNSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@\""  entity "\"];\n"
                                                               "\t[fetchRequest setPredicate:" "[NSPredicate predicateWithFormat:@\"" key-path " = " "%@\"" ", " "object]];\n"
-                                                              "\treturn fetchRequest;\n}\n"]))
+                                                              "\treturn fetchRequest;\n}\n"))
 
-  (defn int-class-import [class-name] (apply str ["#import \"" class-name ".h\"\n"]))
+  (defn int-class-import [class-name] (str "#import \"" class-name ".h\"\n"))
 
-  (def int-imports (apply str ["#import <CoreData/CoreData.h>\n" "#import <Foundation/Foundation.h>\n\n"]))
+  (def int-imports (str "#import <CoreData/CoreData.h>\n" "#import <Foundation/Foundation.h>\n\n"))
 
-  (defn imp-imports [class-name] (apply str ["#import" "\"" class-name "+_FetchRequests.h\"\n\n"]))
+  (defn imp-imports [class-name] (str "#import" "\"" class-name "+_FetchRequests.h\"\n\n"))
 
   (try
-    (def content (let [model-name (first args)] (xml-from-file (apply str [model-name ".xcdatamodeld/" model-name ".xcdatamodel/contents"]))))
+    (def content (let [model-name (first args)] (xml-from-file (str model-name ".xcdatamodeld/" model-name ".xcdatamodel/contents"))))
     (catch Exception e (println (.getMessage e)) (System/exit 0))
-    )
+  )
 
-  (defn is-equal-from-keypath-int [key-path] (apply str ["+ (NSFetchRequest *)" key-path "IsEqualTo:(id)object;\n"]))
+  (defn is-equal-from-keypath-int [key-path] (str "+ (NSFetchRequest *)" key-path "IsEqualTo:(id)object;\n"))
 
-  (defn is-equal-from-keypath-imp-dec [key-path] (apply str ["\n+ (NSFetchRequest *)" key-path "IsEqualTo:(id)object {\n"]))
+  (defn is-equal-from-keypath-imp-dec [key-path] (str "\n+ (NSFetchRequest *)" key-path "IsEqualTo:(id)object {\n"))
 
   (defn is-equal-from-keypath-imp [entity key-path] (imp-with-entity-keypath entity key-path))
 
-  (defn int-file-name-from-class [class] (apply str [class "+_FetchRequests.h"]))
+  (defn int-file-name-from-class [class] (str class "+_FetchRequests.h"))
 
-  (defn imp-file-name-from-class [class] (apply str [class "+_FetchRequests.m"]))
+  (defn imp-file-name-from-class [class] (str class "+_FetchRequests.m"))
 
-  (defn inteface-dec [class-name] (apply str ["@interface " class-name " (_FetchRequests)\n\n"]))
+  (defn inteface-dec [class-name] (str "@interface " class-name " (_FetchRequests)\n\n"))
 
-  (defn imp-dec [class-name] (apply str ["@implementation " class-name " (_FetchRequests)\n"]))
+  (defn imp-dec [class-name] (str "@implementation " class-name " (_FetchRequests)\n"))
 
    (doseq [lines content]
      (doseq [other (:content lines)]
