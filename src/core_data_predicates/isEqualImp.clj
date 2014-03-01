96e810c78146d04421fba23d73b9798a9927729e
Terry Lewis <terrylewis@outlook.com>
2014-03-01 14:23:09 -0600

    new method sig. Now passing MOC, sort descriptor and err block.

--- src/core_data_predicates/isEqualImp.clj
+++ src/core_data_predicates/isEqualImp.clj
@@ -1,9 +1,17 @@
 (ns core-data-predicates.isEqualImp)
 
-(defn is-equal-from-keypath-imp-dec [key-path operation] (str "\n+ (NSFetchRequest *)" key-path operation ":(id)object inContext:(NSManagedObjectContext *)context {\n"))
+(defn is-equal-from-keypath-imp-dec [key-path operation] (str "\n+ (NSArray *)" key-path operation ":(id)object inContext:(NSManagedObjectContext *)context"
+                                                              " sortDescriptors:(NSArray *)sort error:(void(^)(NSError *error))error {\n"))
 
 (defn imp-with-entity-keypath [entity key-path operator] (str "\tNSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@\""  entity "\"];\n"
                                                               "\t[fetchRequest setPredicate:" "[NSPredicate predicateWithFormat:@\"" key-path " " operator  " %@\"" ", " "object]];\n"
-                                                              "\treturn fetchRequest;\n}\n"))
+                                                              "\t[fetchRequest setSortDescriptors:sort];\n"
+                                                              "\tNSError *err = nil;\n"
+                                                              "\tNSArray *results = [context executeFetchRequest:fetchRequest error:&err];\n"
+                                                              "\tif(err && error) {\n"
+                                                              "\t\terror(err);\n"
+                                                              "\t\treturn nil;\n"
+                                                              "\t}\n"
+                                                              "\treturn results;\n}\n"))
 
 (defn is-equal-from-keypath-imp [entity key-path operator] (imp-with-entity-keypath entity key-path operator))
--- src/core_data_predicates/isEqualInt.clj
+++ src/core_data_predicates/isEqualInt.clj
@@ -1,4 +1,5 @@
 (ns core-data-predicates.isEqualInt)
 
-(defn is-equal-from-keypath-int [key-path operation] (str "+ (NSFetchRequest *)" key-path operation
-                                                          ":(id)object inContext:(NSManagedObjectContext *)context;\n"))
+(defn is-equal-from-keypath-int [key-path operation] (str "+ (NSArray *)" key-path operation
+                                                          ":(id)object inContext:(NSManagedObjectContext *)context"
+                                                          " sortDescriptors:(NSArray *)sort error:(void(^)(NSError *error))error;\n"))
